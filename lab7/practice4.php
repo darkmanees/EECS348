c2b34bda1de444b14c7ebce1f37f0eecf0cbc70e
darkmanees <66260868+darkmanees@users.noreply.github.com>
2023-10-30 16:35:04 -0500

    Updated practice4.php

--- lab7/practice4.php
+++ lab7/practice4.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+ 
+<html>
+<body>
+  <h1>Here is your Multiples Table </h1>
+  <a href="practice4.html">Back</a>
 
+ <!-- <table border='2'></table> -->
+  <?php
+    $size= $_POST['size'];
+    
+    echo "<table border =\"1\">";
+    for ($row=0; $row<=$size; $row++){
+        echo "<tr> \n";
+        for ($col=0;$col<=$size; $col++){
+            if ($row==0 && $col==0){
+                echo "<td> </td>";
+
+            }
+            else if($row==0 && $col!=0){
+                echo "<td> $col </td>";
+            }
+            else if($row!=0 && $col==0){
+                echo "<td> $row </td>";
+            }
+            else {
+                $product= $row*$col;
+                echo "<td>$product</td> \n";
+            }
+        }
+        echo "</tr>";
+
+    }
+    
+  ?>
+</body>
+</html>
